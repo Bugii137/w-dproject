ceb2c043faffc9adbfede435eff734861279601e
Mark Kabugi <kabug222@gmail.com>
2025-08-26 22:23:50 +0300

    Oopen weather API key

--- weather-dashboard/tailwind.config.js
+++ weather-dashboard/tailwind.config.js
@@ -5,11 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 1.5s linear infinite',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
